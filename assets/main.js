ab62f6b7eb2e18ba99a3276d9aaf86a8a945e3f0
NaitLee <naitli@foxmail.com>
2024-01-27 05:11:32 +0800

    better server script; fix sometimes cannot init page

--- assets/main.js
+++ assets/main.js
@@ -30,13 +30,15 @@ const moves = {
     x: 0,
     y: 0
 };
-document.addEventListener('DOMContentLoaded', function () {
+
+function start() {
     const docquery = mkquery(document);
     const iframe = docquery('iframe');
     const buttons_e = docquery('#buttons');
     const state_e = docquery('#state');
     const overlay_e = docquery('#overlay');
     const food_e = docquery('#food');
+
     const food_offset = 24;
     overlay_e.style.display = 'none';
 
@@ -86,202 +88,202 @@ document.addEventListener('DOMContentLoaded', function () {
     overlay_e.addEventListener('touchmove', move_go);
     overlay_e.addEventListener('touchend', move_end);
 
-    iframe.addEventListener('load', function () {
-        // iframe.contentWindow.scroll(iframe.contentWindow.scrollMaxX / 2, 0);
-        const svgdoc = iframe.contentDocument;
-        const svgquery = mkquery(svgdoc);
-        const mouths = {
-            mouth: svgquery('#mouth'),
-            tongle: svgquery('#mouth-tongle'),
-            open: svgquery('#mouth-open'),
-            chew: [svgquery('#mouth-chew-1'), svgquery('#mouth-chew-2'), svgquery('#mouth-chew-3')],
-        };
-        const eyes = [ svgquery('#look-1'), svgquery('#look-2') ];
-        const all_mouth = Object.values(mouths).flat();
-        let chew_interval = 0;
-        let chew_index = 0;
+    // iframe.contentWindow.scroll(iframe.contentWindow.scrollMaxX / 2, 0);
+    const svgdoc = iframe.contentDocument;
+    const svgquery = mkquery(svgdoc);
+    const mouths = {
+        mouth: svgquery('#mouth'),
+        tongle: svgquery('#mouth-tongle'),
+        open: svgquery('#mouth-open'),
+        chew: [svgquery('#mouth-chew-1'), svgquery('#mouth-chew-2'), svgquery('#mouth-chew-3')],
+    };
+    const eyes = [ svgquery('#look-1'), svgquery('#look-2') ];
+    const all_mouth = Object.values(mouths).flat();
+    let chew_interval = 0;
+    let chew_index = 0;
 
-        function initFun() {
-            const margin = Math.max(128, window.innerWidth * 0.2, window.innerHeight * 0.2);
-            const e = food_e;
-            overlay_e.style.display = 'block';
-            e.innerText = foods[Math.random() * foods.length | 0];
-            food.x = 150 + (Math.random() * (window.innerWidth - margin) | 0);
-            food.y = 150 + (Math.random() * (window.innerHeight - margin) | 0);
-            e.style.top = food.y - food_offset + 'px';
-            e.style.left = food.x - food_offset + 'px';
-            e.classList.remove('disappeared');
-            food.available = true;
-            look_at(food.x, food.y, true);
+    function initFun() {
+        const margin = Math.max(128, window.innerWidth * 0.2, window.innerHeight * 0.2);
+        const e = food_e;
+        overlay_e.style.display = 'block';
+        e.innerText = foods[Math.random() * foods.length | 0];
+        food.x = 150 + (Math.random() * (window.innerWidth - margin) | 0);
+        food.y = 150 + (Math.random() * (window.innerHeight - margin) | 0);
+        e.style.top = food.y - food_offset + 'px';
+        e.style.left = food.x - food_offset + 'px';
+        e.classList.remove('disappeared');
+        food.available = true;
+        look_at(food.x, food.y, true);
+    }
+    let smooth_on = false;
+    function look_at(x, y, smooth) {
+        const svg_offset = iframe.getBoundingClientRect();
+        const eye_0 = eyes[0].getBoundingClientRect();
+        const eye_1 = eyes[1].getBoundingClientRect();
+        if (smooth && !smooth_on) {
+            eyes.forEach(e => e.classList.add('smooth'));
+            smooth_on = true;
+        } else if (!smooth && smooth_on) {
+            eyes.forEach(e => e.classList.remove('smooth'));
+            smooth_on = false;
         }
-        let smooth_on = false;
-        function look_at(x, y, smooth) {
-            const svg_offset = iframe.getBoundingClientRect();
-            const eye_0 = eyes[0].getBoundingClientRect();
-            const eye_1 = eyes[1].getBoundingClientRect();
-            if (smooth && !smooth_on) {
-                eyes.forEach(e => e.classList.add('smooth'));
-                smooth_on = true;
-            } else if (!smooth && smooth_on) {
-                eyes.forEach(e => e.classList.remove('smooth'));
-                smooth_on = false;
-            }
-            eyes[0].style.transform = `translate(${
-                Math.max(-1.0, Math.min(1.6, ((x + moves.x) - (svg_offset.x + eye_0.x + eye_0.width / 2)) * 0.004))
-            }px, ${
-                Math.max(-1.2, Math.min(1.2, ((y + moves.y) - (svg_offset.y + eye_0.y + eye_0.height / 2)) * 0.002))
-            }px)`;
-            eyes[1].style.transform = `translate(${
-                Math.max(-0.4, Math.min(2.0, ((x + moves.x) - (svg_offset.x + eye_1.x + eye_1.width / 2)) * 0.004))
-            }px, ${
-                Math.max(-1.2, Math.min(1.2, ((y + moves.y) - (svg_offset.y + eye_1.y + eye_1.height / 2)) * 0.002))
-            }px)`;
+        eyes[0].style.transform = `translate(${
+            Math.max(-1.0, Math.min(1.6, ((x + moves.x) - (svg_offset.x + eye_0.x + eye_0.width / 2)) * 0.004))
+        }px, ${
+            Math.max(-1.2, Math.min(1.2, ((y + moves.y) - (svg_offset.y + eye_0.y + eye_0.height / 2)) * 0.002))
+        }px)`;
+        eyes[1].style.transform = `translate(${
+            Math.max(-0.4, Math.min(2.0, ((x + moves.x) - (svg_offset.x + eye_1.x + eye_1.width / 2)) * 0.004))
+        }px, ${
+            Math.max(-1.2, Math.min(1.2, ((y + moves.y) - (svg_offset.y + eye_1.y + eye_1.height / 2)) * 0.002))
+        }px)`;
+    }
+    function look_pointer(event) {
+        const touch = event.touches ? event.touches[0] : event;
+        if (moves.start_x !== moves.prev_x || moves.start_y !== moves.prev_y) {
+            return;
         }
-        function look_pointer(event) {
-            const touch = event.touches ? event.touches[0] : event;
-            if (moves.start_x !== moves.prev_x || moves.start_y !== moves.prev_y) {
-                return;
-            }
-            look_at(moves.start_x, moves.start_y, true);
-            setTimeout(() => look_at(food.x + moves.x, food.y + moves.y, true), 800 + 800 * Math.random());
-        }
+        look_at(moves.start_x, moves.start_y, true);
+        setTimeout(() => look_at(food.x + moves.x, food.y + moves.y, true), 800 + 800 * Math.random());
+    }
 
-        const food_detect = function(event) {
-            event.preventDefault();
-            if (!moves.on || !food.available) return;
-            const svg_offset = iframe.getBoundingClientRect();
-            let mouth = mouths.open.getBoundingClientRect();
-            if (mouth.x === 0 && mouth.y === 0 && mouth.x === 0 && mouth.y === 0)
-                mouth = mouths.mouth.getBoundingClientRect();
-            look_at(food.x, food.y, false);
-            mouths.open.style.display = (
-                food.x + moves.x < mouth.x + svg_offset.x - food_offset * 4 ||
-                food.y + moves.y < mouth.y + svg_offset.y - food_offset * 3 ||
-                food.x + moves.x > mouth.x + svg_offset.x + mouth.width + food_offset * 4 ||
-                food.y + moves.y > mouth.y + svg_offset.y + mouth.height + food_offset * 3
-            ) ? 'none' : 'inline';
-            mouths.mouth.style.display = mouths.open.style.display === 'none' ? 'inline': 'none';
-        };
-        const food_eat = function(event) {
-            event.preventDefault();
-            if (!food.available) return;
-            const svg_offset = iframe.getBoundingClientRect();
-            const mouth = mouths.open.getBoundingClientRect();
-            mouths.open.style.display = 'none';
-            if (
-                food.x < mouth.x + svg_offset.x - food_offset ||
-                food.y < mouth.y + svg_offset.y - food_offset ||
-                food.x > mouth.x + svg_offset.x + mouth.width + food_offset ||
-                food.y > mouth.y + svg_offset.y + mouth.height + food_offset
-            ) {
-                mouths.mouth.style.display = 'inline';
-                return;
-            }
-            chew_index = 0;
-            food.available = false;
-            mouths.chew[chew_index].style.display = 'inline';
-            // food_e.innerText = '';
-            food_e.classList.add('disappeared');
-            eyes[0].style.transform = eyes[1].style.transform = '';
+    const food_detect = function(event) {
+        event.preventDefault();
+        if (!moves.on || !food.available) return;
+        const svg_offset = iframe.getBoundingClientRect();
+        let mouth = mouths.open.getBoundingClientRect();
+        if (mouth.x === 0 && mouth.y === 0 && mouth.x === 0 && mouth.y === 0)
+            mouth = mouths.mouth.getBoundingClientRect();
+        look_at(food.x, food.y, false);
+        mouths.open.style.display = (
+            food.x + moves.x < mouth.x + svg_offset.x - food_offset * 4 ||
+            food.y + moves.y < mouth.y + svg_offset.y - food_offset * 3 ||
+            food.x + moves.x > mouth.x + svg_offset.x + mouth.width + food_offset * 4 ||
+            food.y + moves.y > mouth.y + svg_offset.y + mouth.height + food_offset * 3
+        ) ? 'none' : 'inline';
+        mouths.mouth.style.display = mouths.open.style.display === 'none' ? 'inline': 'none';
+    };
+    const food_eat = function(event) {
+        event.preventDefault();
+        if (!food.available) return;
+        const svg_offset = iframe.getBoundingClientRect();
+        const mouth = mouths.open.getBoundingClientRect();
+        mouths.open.style.display = 'none';
+        if (
+            food.x < mouth.x + svg_offset.x - food_offset ||
+            food.y < mouth.y + svg_offset.y - food_offset ||
+            food.x > mouth.x + svg_offset.x + mouth.width + food_offset ||
+            food.y > mouth.y + svg_offset.y + mouth.height + food_offset
+        ) {
+            mouths.mouth.style.display = 'inline';
+            return;
+        }
+        chew_index = 0;
+        food.available = false;
+        mouths.chew[chew_index].style.display = 'inline';
+        // food_e.innerText = '';
+        food_e.classList.add('disappeared');
+        eyes[0].style.transform = eyes[1].style.transform = '';
+        svgquery('#eyes').style.display = 'none';
+        svgquery('#winks-down').style.display = 'inline';
+        chew_interval = setInterval(function() {
+            mouths.chew[chew_index].style.display = 'none';
+            mouths.chew[(chew_index + 1) % mouths.chew.length].style.display = 'inline';
+            chew_index = (chew_index + 1) % mouths.chew.length;
+        }, 200);
+        // let feeds = parseInt(localStorage.getItem('feeds') || '0');
+        fetch('/~feed');
+        docquery('#feeds').innerText = ++feeds;
+        localStorage.setItem('feeds', feeds);
+        setTimeout(function() {
+            clearInterval(chew_interval);
+            mouths.chew[chew_index].style.display = 'none';
+            for (const e of all_mouth)
+                e.style.display = 'none';
+            mouths.mouth.style.display = mouths.tongle.style.display = 'inline';
             svgquery('#eyes').style.display = 'none';
-            svgquery('#winks-down').style.display = 'inline';
-            chew_interval = setInterval(function() {
-                mouths.chew[chew_index].style.display = 'none';
-                mouths.chew[(chew_index + 1) % mouths.chew.length].style.display = 'inline';
-                chew_index = (chew_index + 1) % mouths.chew.length;
-            }, 200);
-            // let feeds = parseInt(localStorage.getItem('feeds') || '0');
-            fetch('/~feed');
-            docquery('#feeds').innerText = ++feeds;
-            localStorage.setItem('feeds', feeds);
+            svgquery('#winks-down').style.display = 'none';
+            svgquery('#winks').style.display = 'inline';
+            eyes.forEach(e => e.style.transform = '');
             setTimeout(function() {
-                clearInterval(chew_interval);
-                mouths.chew[chew_index].style.display = 'none';
-                for (const e of all_mouth)
-                    e.style.display = 'none';
-                mouths.mouth.style.display = mouths.tongle.style.display = 'inline';
-                svgquery('#eyes').style.display = 'none';
-                svgquery('#winks-down').style.display = 'none';
-                svgquery('#winks').style.display = 'inline';
-                eyes.forEach(e => e.style.transform = '');
+                mouths.tongle.style.display = 'none';
+                svgquery('#eyes').style.display = 'inline';
+                svgquery('#winks').style.display = 'none';
                 setTimeout(function() {
-                    mouths.tongle.style.display = 'none';
-                    svgquery('#eyes').style.display = 'inline';
-                    svgquery('#winks').style.display = 'none';
-                    setTimeout(function() {
-                        initFun();
-                    }, 1000 + Math.random() * 2000);
-                }, 2000);
-            }, 4 * 3 * 200);
-        };
-        overlay_e.addEventListener('mousemove', food_detect);
-        overlay_e.addEventListener('mouseup', food_eat);
-        overlay_e.addEventListener('mouseup', look_pointer);
-        overlay_e.addEventListener('touchmove', food_detect);
-        overlay_e.addEventListener('touchend', food_eat);
-        overlay_e.addEventListener('touchend', look_pointer);
+                    initFun();
+                }, 1000 + Math.random() * 2000);
+            }, 2000);
+        }, 4 * 3 * 200);
+    };
+    overlay_e.addEventListener('mousemove', food_detect);
+    overlay_e.addEventListener('mouseup', food_eat);
+    overlay_e.addEventListener('mouseup', look_pointer);
+    overlay_e.addEventListener('touchmove', food_detect);
+    overlay_e.addEventListener('touchend', food_eat);
+    overlay_e.addEventListener('touchend', look_pointer);
 
-        const showstate = () => state_e.innerText = JSON.stringify(state, void 0, 4);
-        const addbutton = function(id, onclick, strong) {
-            const button = document.createElement('button');
-            if (strong) button.classList.add('strong')
-            button.innerText = id;
-            button.addEventListener('click', onclick);
-            button.addEventListener('click', showstate);
-            buttons_e.appendChild(button);
-        }
-        const display = (boolean) => boolean ? 'inline' : 'none';
-        const choices = {
-            eyebrow: ['#eyebrow', '#eyebrow-alt'],
-            mouth: ['#mouth', '#mouth-alt']
-        };
-        addbutton('look', function () {
-            state.look = !state.look;
-            svgquery('#look-1').style.transform = state.look ? '' : 'translateX(1.2px)';
-            svgquery('#look-2').style.transform = state.look ? '' : 'translateX(1.6px)';
-        });
-        addbutton('flip', function () {
-            state.flip = -state.flip;
-            svgquery('#nait').style.transformOrigin = '50% 0';
-            svgquery('#nait').style.transform = `scaleX(${state.flip})`;
-        });
-        for (const item of ['visor', 'glasses', 'hairtip'])
-            addbutton(item, function () {
-                state[item] = !state[item];
-                svgquery('#' + item).style.display = display(state[item]);
-            });
-        for (const item of ['eyebrow'])
-            addbutton(item, function () {
-                for (const choice of choices[item])
-                    svgquery(choice).style.display = display(false);
-                state[item]++;
-                state[item] %= choices[item].length;
-                svgquery(choices[item][state[item]]).style.display = display(true);
-            });
-        /*
-        addbutton('save', function () {
-            const timestamp = new Date();
-            Object.assign(document.createElement('a'), {
-                download: `nait-b-${timestamp.getTime()}.svg`,
-                href: URL.createObjectURL(new Blob([
-                    '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n',
-                    svgdoc.rootElement.outerHTML
-                ]))
-            }).click();
-        });
-        */
-        addbutton('feed', function () {
-            state.fun = true;
-            setTimeout(function() {
-                docquery('aside').remove();
-                document.body.style.textAlign = 'center';
-                document.body.style.overflow = 'hidden';
-                scrollTo(0, 0);
-                initFun();
-            }, 1000);
-        }, true);
-        showstate();
+    const showstate = () => state_e.innerText = JSON.stringify(state, void 0, 4);
+    const addbutton = function(id, onclick, strong) {
+        const button = document.createElement('button');
+        if (strong) button.classList.add('strong')
+        button.innerText = id;
+        button.addEventListener('click', onclick);
+        button.addEventListener('click', showstate);
+        buttons_e.appendChild(button);
+    }
+    const display = (boolean) => boolean ? 'inline' : 'none';
+    const choices = {
+        eyebrow: ['#eyebrow', '#eyebrow-alt'],
+        mouth: ['#mouth', '#mouth-alt']
+    };
+    addbutton('look', function () {
+        state.look = !state.look;
+        svgquery('#look-1').style.transform = state.look ? '' : 'translateX(1.2px)';
+        svgquery('#look-2').style.transform = state.look ? '' : 'translateX(1.6px)';
     });
-});
+    addbutton('flip', function () {
+        state.flip = -state.flip;
+        svgquery('#nait').style.transformOrigin = '50% 0';
+        svgquery('#nait').style.transform = `scaleX(${state.flip})`;
+    });
+    for (const item of ['visor', 'glasses', 'hairtip'])
+        addbutton(item, function () {
+            state[item] = !state[item];
+            svgquery('#' + item).style.display = display(state[item]);
+        });
+    for (const item of ['eyebrow'])
+        addbutton(item, function () {
+            for (const choice of choices[item])
+                svgquery(choice).style.display = display(false);
+            state[item]++;
+            state[item] %= choices[item].length;
+            svgquery(choices[item][state[item]]).style.display = display(true);
+        });
+    /*
+    addbutton('save', function () {
+        const timestamp = new Date();
+        Object.assign(document.createElement('a'), {
+            download: `nait-b-${timestamp.getTime()}.svg`,
+            href: URL.createObjectURL(new Blob([
+                '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n',
+                svgdoc.rootElement.outerHTML
+            ]))
+        }).click();
+    });
+    */
+    addbutton('feed', function () {
+        state.fun = true;
+        setTimeout(function() {
+            docquery('aside').remove();
+            document.body.style.textAlign = 'center';
+            document.body.style.overflow = 'hidden';
+            scrollTo(0, 0);
+            initFun();
+        }, 1000);
+    }, true);
+    showstate();
+}
+
+document.addEventListener('DOMContentLoaded', start);
 
 // @license-end
--- main.ts
+++ main.ts
@@ -1,49 +1,49 @@
 /// <reference lib="deno.unstable" />
 
-import { Status, STATUS_TEXT } from "https://deno.land/std@0.192.0/http/http_status.ts";
-
 const KV_KEY_FEEDS = ['feeds'];
 
-const config = JSON.parse(await Deno.readTextFile('config.json'));
+const config = {
+    "host": "0.0.0.0",
+    "port": 8291
+};
 
 const api_to_mode = {
     '~getfeeds': 'get',
     '~feed': 'hit'
 }
 
+function extname(path: string) {
+    const segs = path.split('.');
+    if (segs.length === 1) return '';
+    return segs[segs.length - 1];
+}
+
+const Mime = {
+    'html': 'text/html;charset=utf-8',
+    'svg': 'image/svg+xml;charset=utf-8',
+    'css': 'text/css;charset=utf-8',
+    'js': 'text/javascript;charset=utf-8',
+    'json': 'text/json;charset=utf-8',
+    'ico': 'image/vnd.microsoft.icon'
+};
+
 Deno.serve({
     hostname: config.host,
     port: config.port
 }, async function (request: Request) {
     const url = new URL(request.url);
-    if (!['HEAD', 'GET', 'POST'].includes(request.method))
-        return new Response(null, {
-            status: Status.MethodNotAllowed,
-            statusText: STATUS_TEXT[Status.MethodNotAllowed]
-        });
     let path = url.pathname.slice(1);
     if (path === '')
         path = 'index.html';
-    if (/\.(ts|jsx|tsx|tsm|exe|msi|chm|bat|cmd|php|jsp|aspx?)$/.test(path) || path.includes('/..')) {
-        return new Response(null, {
-            status: Status.Teapot,
-            statusText: STATUS_TEXT[Status.Teapot]
-        });
-    }
     if (path === 'nait-fun.html')
         return new Response(null, {
-            status: Status.MovedPermanently,
-            statusText: STATUS_TEXT[Status.MovedPermanently],
-            headers: {
-                'Location': '/'
-            }
+            status: 301,
+            headers: { 'Location': '/' }
         });
     if (path.startsWith('~')) {
         const mode = api_to_mode[path as keyof typeof api_to_mode];
         if (mode === undefined)
-            return new Response(STATUS_TEXT[Status.NotImplemented], {
-                status: Status.NotImplemented
-            });
+            return new Response(null, { status: 400 });
         const kv = await Deno.openKv();
         const entry_feeds = await kv.get<number>(KV_KEY_FEEDS);
         let feeds;
@@ -58,14 +58,12 @@ Deno.serve({
                     status: Status.NoContent
                 });
             case 'get':
-                return new Response(JSON.stringify({
-                    'value': feeds
-                }), {
-                    headers: {
-                        'Content-Type': 'application/json;charset=utf-8'
-                    }
+                return new Response(JSON.stringify({ 'value': feeds }), {
+                    headers: { 'Content-Type': Mime['json'] }
                 });
         }
     }
-    return fetch(new URL(path, import.meta.url));
+    return fetch(new URL(path, import.meta.url)).then(r => r.body).then(stream => new Response(stream, {
+        headers: { 'Content-Type': Mime[extname(path)] || 'text/plain' }
+    })).catch(_ => new Response('not found', { status: 404 }));
 });
